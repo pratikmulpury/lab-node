eba145e4781408a1cae0b47c441b2837e5dacdc5
Robert Duvall <rcd@cs.duke.edu>
2018-03-26 10:53:56 -0400

    Initial code files

---
+++ src/App.vue
@@ -0,0 +1,38 @@
+<template>
+    <div id="app">
+        <h1>My Favorite Sites</h1>
+        <ul id="js-linklist">
+            <url-list v-for="item in items" :key="item.title" :item="item"></url-list>
+        </ul>
+    </div>
+</template>
+
+<script>
+// load another component to be used in the standard Vue object
+import UrlList from './components/URLList.vue'
+// load JSON file driectly as JavaScript data structure, not recommended for LARGE data files
+import LINKS from './assets/nested_links.json'
+
+// export anonymous object from this module so it can be accessed by others when imported
+export default {
+    name: 'app',
+    // NOTE in a component, data must be a function that returns a NEW version of the values
+    data () {
+        return {
+            items: LINKS
+        }
+    },
+    // components (HTML, CSS, and JS) used by this app
+    components: {
+        UrlList
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+$header-color: hotpink;
+
+h1 {
+    color: $header-color;
+}
+</style>
---
+++ src/components/URLList.vue
@@ -0,0 +1,44 @@
+<template>
+    <!-- template must have a SINGLE root tag that encloses all others -->
+    <li class="title">{{ item.title }}
+        <ul class="category">
+            <li class="link" v-for="link in item.links">
+                <a :href="link.url">{{ link.name }}</a>
+            </li>
+        </ul>
+    </li>
+</template>
+
+<script>
+// export anonymous object from this module so it can be accessed by others when imported
+export default {
+    name: 'URLList',
+    props: [ 'item' ]
+}
+</script>
+
+<style lang="scss" scoped>
+/* NOTE, scoped limits style to ONLY tags within this template */
+$link-color: darkorange;
+$item-color: lightseagreen;
+$bullet-color: darken($item-color, 8%);
+
+a {
+    color: $link-color;
+}
+
+li.title {
+    color: $item-color;
+    list-style: none;
+    margin: 20px 0 0 0;
+    font-size: 18px;
+    font-weight: bold;
+}
+
+li.link {
+    color: $bullet-color;
+    list-style: square;
+    font-weight: normal;
+    font-size: 14px;
+}
+</style>
