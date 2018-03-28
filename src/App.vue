12ae31139b4222d975e3371804bdc2901bb54a3a
Robert Duvall <rcd@cs.duke.edu>
2018-03-28 13:29:32 -0400

    Changed the example for this lab

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,83 @@
 <template>
-    <div id="app">
-        <h1>My Favorite Sites</h1>
-        <ul id="js-linklist">
-            <url-list v-for="item in items" :key="item.title" :item="item"></url-list>
-        </ul>
+<!-- This HTML code does not need to be changed, but you should move it around to break it up into components. -->
+<div id="app" class="container">
+    <div class="page-header">
+        <h1>My Favorite Books</h1>
     </div>
+    <div class="panel panel-default">
+        <div class="panel-heading">
+            <h3 class="panel-title">Add New Books</h3>
+        </div>
+        <div class="panel-body">
+            <form id="form" class="form-inline" @submit.prevent="addBook">
+                <div class="form-group">
+                    <label for="bookTitle">Title:</label>
+                    <input type="text" id="bookTitle" class="form-control" v-model="newBookTitle">
+                </div>
+                <div class="form-group">
+                    <label for="bookAuthor">Author:</label>
+                    <input type="text" id="bookAuthor" class="form-control" v-model="newBookAuthor">
+                </div>
+                <input type="submit" class="btn btn-primary" value="Add Book">
+            </form>
+        </div>
+    </div>
+    <div class="panel panel-default">
+        <div class="panel-heading">
+            <h3 class="panel-title">Book List</h3>
+        </div>
+        <div class="panel-body">
+            <table class="table">
+                <thead>
+                    <tr>
+                        <th class="first"></th>
+                        <th>Title</th>
+                        <th>Author</th>
+                        <th>Year</th>
+                        <th></th>
+                    </tr>
+                </thead>
+                <!-- TODO: make this element into a new component template -->
+                <tbody v-for="book in books">
+                    <tr>
+                        <td class="first" rowspan="2"><img :src="book.imgUrl" /></td>
+                        <td class="top"><a :href="book.url">{{book.title}}</a></td>
+                        <td class="top">{{book.author}}</td>
+                        <td class="top">{{book.year}}</td>
+                        <td class="top">
+                            <span class="glyphicon glyphicon-trash"
+                                  aria-hidden="true"
+                                  @click="removeBook(book)">
+                            </span>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td colspan="3">{{book.description}}</td>
+                        <td></td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    </div>
+</div>
 </template>
 
 <script>
-// load another component to be used in the standard Vue object
-import UrlList from './components/URLList.vue'
-// load JSON file driectly as JavaScript data structure, not recommended for LARGE data files
-import LINKS from './assets/nested_links.json'
+// This code will NEED to updated to use the JSON data structure returned from the Google Books API
+
+// use Firebase to save data user entered
+import Firebase from 'firebase';
+
+var config = {
+    apiKey: "AIzaSyALmW2B2aJOtwCbikL6fI3RyfZejpKvgYI",
+    authDomain: "book-list-70bcc.firebaseapp.com",
+    databaseURL: "https://book-list-70bcc.firebaseio.com",
+    projectId: "book-list-70bcc",
+    storageBucket: "book-list-70bcc.appspot.com",
+    messagingSenderId: "167870498358"
+}
+var db = Firebase.initializeApp(config).database()
+var booksRef = db.ref('books')
 
 // export anonymous object from this module so it can be accessed by others when imported
 export default {
@@ -19,20 +85,95 @@ export default {
     // NOTE in a component, data must be a function that returns a NEW version of the values
     data () {
         return {
-            items: LINKS
+            newBookTitle: '',
+            newBookAuthor: ''
         }
+    },
+    // connect to single Firebase resource
+    firebase: {
+        books: booksRef
     },
     // components (HTML, CSS, and JS) used by this app
     components: {
-        UrlList
+        // TODO: add your new component(s) here
+    },
+    // functions you want to be called from HTML code
+    methods: {
+        // add book to the database after getting details from Google Books API
+        addBook () {
+            this.getBookDetails(this.newBookTitle, this.newBookAuthor)
+            this.resetInputValues()
+        },
+        // remove book from the database
+        removeBook (book) {
+            booksRef.child(book['.key']).remove()
+        },
+        // given title and author, get JSON data about the book to display
+        getBookDetails (title, author) {
+            // encode user input for inclusion within URL
+            var encodedTitle = encodeURIComponent(title),
+                encodedAuthor = encodeURIComponent(author),
+                url = `https://www.googleapis.com/books/v1/volumes?q=intitle:${encodedTitle}+inauthor:${encodedAuthor}&maxResults=1`
+            // get JSON data to fill in specific book data
+            fetch(url).then(response => response.json())
+                      .then(data => {
+                            // only one result is returned to reduce data load, but it still is returned as an Array
+                            var bookInfo = data.items[0]
+                            // TODO: replace dummy data below with correct fields within given JSON data
+                            booksRef.push({
+                                title: title,
+                                author: author,
+                                year: '2001',                    // TODO: find book's publication date
+                                description: 'Interesting book', // TODO: find book's description
+                                url: 'http://google.com',        // TODO: find link to the book's information
+                                imgUrl: 'https://files.selar.co/product-images/2017/products/demo/preorder-physical-product-selar.co-59d1e525c3a96.jpg',              // TODO: find link to the book's thumbnail image
+                            })
+                       })
+                      .catch(error => console.log(error))
+        },
+        // clear input form to prepare for the next entry
+        resetInputValues () {
+            this.newBookTitle = ''
+            this.newBookAuthor = ''
+        }
     }
 }
 </script>
 
 <style lang="scss" scoped>
-$header-color: hotpink;
+/*
+ * This CSS code will need to be changed to verify components have different styles.
+ * You will also need to move it around to break it up into components.
+ */
 
-h1 {
-    color: $header-color;
+/* TODO: move styles to the appropriate component where they would most closely apply */
+$formColor: cornflowerblue;
+$submitColor: hotpink;
+$tableColor: rebeccapurple;
+
+#app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    color: $formColor;
+    margin-top: 20px;
+}
+
+.btn-primary {
+    background-color: $submitColor;
+}
+
+.first {
+    width: 1%;
+}
+
+.top {
+    height: 1.5em;
+}
+
+img {
+    height: 200px;
+}
+
+.table {
+    color: $tableColor;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -4,10 +4,14 @@ import 'bootstrap/dist/css/bootstrap.css'
 import './assets/css/style.css'
 
 import Vue from 'vue'
+import VueFire from 'vuefire'
 import App from './App.vue'
 
 // turn off the console note about switching to production mode
 Vue.config.productionTip = false
+
+// explicit installation required in module environments
+Vue.use(VueFire)
 
 // simply creating the Vue instance does all the necessary set up, so no need to name it
 new Vue({
